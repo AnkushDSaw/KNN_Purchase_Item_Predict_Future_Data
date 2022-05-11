2b3eaf7fc1fed32c89816e7ef7d7e889b91566df
AD <91939906+AnkushDSaw@users.noreply.github.com>
2022-05-11 19:55:24 +0530

    Add files via upload
    Social_Network_Ads.csv --- Input file
    future prediction _ 2.csv -- Input file for future data
    KNN_modelpredicated.csv -- Output file for future data

---
+++ Final_k_nearest_neighbors.py
@@ -0,0 +1,101 @@
+# K-Nearest Neighbors (K-NN)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv(r'C:\Users\ankus\OneDrive\Desktop\Naresh IT\May\10_May\9th\Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the K-NN model on the Training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=3, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None, n_jobs=None)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# This is to get the Models Accuracy 
+from sklearn.metrics import accuracy_score 
+ac = accuracy_score(y_test, y_pred)
+print(ac) 
+
+bias=classifier.score(X_train,y_train)
+print(bias)
+
+variance=classifier.score(X_test,y_test)
+print(variance)
+
+
+# This is to get the Classification Report
+from sklearn.metrics import classification_report
+cr = classification_report(y_test, y_pred)
+cr
+
+#******************  . check the future data with predicated data
+furture_dataset=pd.read_csv(r'C:\Users\ankus\OneDrive\Desktop\Naresh IT\May\10_May\9th\future prediction _ 2.csv')
+
+X_new=furture_dataset.iloc[:,[2,3]].values
+X_new=sc.fit_transform(X_new)
+y_Logi_pred=classifier.predict(X_new)
+#********************* 11. Dataframe Creation
+import os
+df1=pd.DataFrame(y_Logi_pred)
+df1=df1.rename(columns={'NAN':'Index', 0:'modelpredicated'})
+df1.to_csv('C:/Users/ankus/OneDrive/Desktop/Naresh IT/May/10_May/9th/KNN_modelpredicated.csv')
+
+
+
+# # Visualising the Training set results
+# from matplotlib.colors import ListedColormap
+# X_set, y_set = X_train, y_train
+# X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+#                       np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+# plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+#               alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+# plt.xlim(X1.min(), X1.max())
+# plt.ylim(X2.min(), X2.max())
+# for i, j in enumerate(np.unique(y_set)):
+#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+#                 c = ListedColormap(('red', 'green'))(i), label = j)
+# plt.title('K-NN (Training set)')
+# plt.xlabel('Age')
+# plt.ylabel('Estimated Salary')
+# plt.legend()
+# plt.show()
+
+# # Visualising the Test set results
+# from matplotlib.colors import ListedColormap
+# X_set, y_set = X_test, y_test
+# X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+#                       np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+# plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+#               alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+# plt.xlim(X1.min(), X1.max())
+# plt.ylim(X2.min(), X2.max())
+# for i, j in enumerate(np.unique(y_set)):
+#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+#                 c = ListedColormap(('red', 'green'))(i), label = j)
+# plt.title('K-NN (Test set)')
+# plt.xlabel('Age')
+# plt.ylabel('Estimated Salary')
+# plt.legend()
+# plt.show()
